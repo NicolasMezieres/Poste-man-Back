056f9ea81a558310895ab68766371643397b2839
Nicolas Mezieres <nicolas.mezieres73@gmail.com>
2025-09-01 19:30:29 +0200

    nest cli add asset prisma client

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // app.use(cookieParser());
+  // app.enableCors({
+  //   origin: [
+  //     'https://lost-web-nicolasmezieres-nicolas-projects-d55648f9.vercel.app',
+  //     'http://localhost:4200',
+  //   ],
+  //   credentials: true,
+  // });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => console.log(err));
