e8d09244f073ded24ae3cb0023604c89fbf18178
Daorse <mmorpgplays5@gmail.com>
2025-09-05 14:07:51 +0200

    Branch create folder add for notification

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { JwtAdminStrategy } from './auth/strategy/admin.strategy';
 import { JwtStrategy } from './auth/strategy/jwt.strategy';
 import { EmailModule } from './email/email.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { NotificationModule } from './notification/notification.module';
 @Module({
   imports: [
     PrismaModule,
@@ -15,6 +16,7 @@ import { PrismaModule } from './prisma/prisma.module';
     JwtModule,
     JwtStrategy,
     JwtAdminStrategy,
+    NotificationModule,
   ],
 })
 export class AppModule {}
