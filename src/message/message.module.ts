095f4ca390388c40b7fa46150576d71bbd95793b
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-10-22 10:22:05 +0200

    add to module message, notification Service

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -3,9 +3,15 @@ import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { MessageGateway } from './message.gateway';
+import { NotificationService } from 'src/notification/notification.service';
 
 @Module({
   controllers: [MessageController],
-  providers: [MessageService, PrismaService, MessageGateway],
+  providers: [
+    MessageService,
+    PrismaService,
+    MessageGateway,
+    NotificationService,
+  ],
 })
 export class MessageModule {}
