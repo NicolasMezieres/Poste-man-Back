e63dbeeba6adf9e9a1d1014498b1fc41f6bd0b32
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-23 21:46:08 +0100

    test 18

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -8,7 +8,6 @@ export default defineConfig({
     seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    url: env('DATABASE_URL') as string,
+    url: process.env.DATABASE_URL as string,
   },
 });
