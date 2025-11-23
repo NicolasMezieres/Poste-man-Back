694c43945d4b4e598f827751389a80f81cc44216
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-23 21:38:36 +0100

    test 16

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    url: 'postgresql://test:password@localhost:5432/testdb',
+    url: process.env.DATABASE_URL as string,
   },
 });
