7c1899d04178aed91a718deed74d8735382de60e
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-24 16:21:28 +0100

    change path prisma

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,17 @@ import { createDefaultPreset } from 'ts-jest';
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
+export const rootDir = '.';
 export const coverageDirectory = '../coverage/';
-export const coveragePathIgnorePatterns = ['src/prisma/generated'];
+export const coveragePathIgnorePatterns = [
+  'src/prisma/generated',
+  'node_modules',
+];
 export const testEnvironment = 'node';
 export const transform = {
   ...tsJestTransformCfg,
 };
 export const moduleNameMapper = {
-  '^src/(.*)$': '<rootDir>/$1',
+  '^src/(.*)$': '<rootDir>/src/$1',
+  '^prisma/(.*)$': '<rootDir>/prisma/$1',
 };
-export const rootDir = 'src';
