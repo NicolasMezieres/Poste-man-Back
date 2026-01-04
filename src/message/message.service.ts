7a919eb25c2c3177f540545757b9456d66eed318
nicolas mezieres <nicolas.mezieres73@gmail.com>
2026-01-04 23:58:42 +0100

    get name project (message)

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -23,6 +23,27 @@ export class MessageService {
     private socket: MessageGateway,
     private notification: NotificationService,
   ) {}
+  async projectName(projectId: string, user: UserWithRole) {
+    const isAdmin = user.role.name === role.ADMIN;
+    if (isAdmin) {
+      const existingProject = await this.prisma.project.findUnique({
+        where: { id: projectId },
+        select: { name: true },
+      });
+      if (!existingProject) {
+        throw new NotFoundException('Project introuvable');
+      }
+      return { projectName: existingProject.name };
+    }
+    const didUserInProject = await this.prisma.user_Has_Project.findFirst({
+      where: { userId: user.id, projectId, isBanned: false },
+      select: { project: { select: { name: true } } },
+    });
+    if (!didUserInProject) {
+      throw new ForbiddenException("Vous n'êtes pas dans le projet");
+    }
+    return { projectName: didUserInProject.project.name };
+  }
   async projectMessages(
     projectId: string,
     user: UserWithRole,
