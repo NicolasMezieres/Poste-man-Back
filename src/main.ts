db99e03a5a5766a7d495f4bc770a90b5ecfddc2b
nicolas mezieres <nicolas.mezieres73@gmail.com>
2026-02-17 23:42:22 +0100

    configuration cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: [`${process.env.FRONT_URL}`],
     credentials: true,
   });
   app.use(cookieParser());
