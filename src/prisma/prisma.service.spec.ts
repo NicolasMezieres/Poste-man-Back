28b81e7aea09a19c87e5f98053b173894a5fb2f1
nicolas mezieres <nicolas.mezieres73@gmail.com>
2026-03-03 09:25:42 +0100

    Prisma unit test

---
+++ src/prisma/prisma.service.spec.ts
@@ -0,0 +1,16 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from './prisma.service';
+import { ConfigService } from '@nestjs/config';
+
+describe('PrismaService', () => {
+  let service: PrismaService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService, ConfigService],
+    }).compile();
+    service = module.get<PrismaService>(PrismaService);
+  });
+  it('Should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
